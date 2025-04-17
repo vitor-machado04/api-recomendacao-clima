b671cf1c4e37b56466e7a562d6b0afe30d292f2e
Vitor Machado <vitor.machado@vtrina.com.br>
2025-04-16 21:17:51 -0300

    Add Redis caching to weather recommendation API

--- api-a/index.js
+++ api-a/index.js
@@ -2,24 +2,48 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 const PORT = 3000;
+const redis = require('redis');
+let client;
 
 app.get('/recommendation/:city', async (req, res) => {
   const city = req.params.city.toLowerCase();
 
   try {
-    const response = await axios.get(`http://localhost:3001/weather/${city}`);
-    const { city: nomeCidade, temp, unit } = response.data;
+    if (!client) {
+      client = redis.createClient({
+        host: 'localhost',
+        port: 6379
+      })
 
-    let suggestion = '';
-    if (temp > 30) {
-      suggestion = 'Está muito quente! Hidrate-se e use protetor solar.';
-    } 
-    else if (temp > 15) {
-      suggestion = 'O clima está agradável!';
-    } 
-    else {
-      suggestion = 'Está frio! Use um casaco.';
+      client.connect();
     }
+
+    const cacheKey = req.params.city;
+    let data = await client.get(cacheKey);
+
+    console.log('Cache:', data);
+
+    data = data !== null ? JSON.parse(data) : null;
+
+    const { city: nomeCidade, temp, unit } = data
+      || await axios.get(`http://localhost:3001/weather/${city}`).then(response => response.data);
+
+    if (!data) {
+      client.set(
+        cacheKey,
+        JSON.stringify({ city: nomeCidade, temp, unit }),
+        {
+          ex: 60
+        }
+      );
+
+      console.log('adicionou cache')
+    }
+
+    let suggestion = 'Está frio! Use um casaco.';
+
+    if (temp > 30) suggestion = 'Está muito quente! Hidrate-se e use protetor solar.';
+    else if (temp > 15) suggestion = 'O clima está agradável!';
 
     res.json({
       city: nomeCidade,
@@ -27,8 +51,7 @@ app.get('/recommendation/:city', async (req, res) => {
       unit,
       recommendation: suggestion
     });
-
-  } 
+  }
   catch (error) {
     res.status(500).json({ error: 'Erro ao obter dados climáticos' });
   }
